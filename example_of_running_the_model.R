b470d163813b4801c3014135635c8e2eef07103a
Frances Cathcart Burn <f.cathcartburn@nhs.net>
2026-02-05 10:37:14 +0000

    ignore local files

--- example_of_running_the_model.R
+++ example_of_running_the_model.R
@@ -1,9 +1,13 @@
 #run example 1
 
+original_wd <- getwd()
 source("run_c_and_d_model_fcn.R")
 source("error_checks_fcn.R")
 wd_subfolder <- "example_1"
 run_c_and_d_model(model_subfolder = wd_subfolder)
+
+##reset working directory
+setwd(original_wd)
 
 ##run files in the main working directory (not in a subfolder)
 #wd_subfolder <- ""
